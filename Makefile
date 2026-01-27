8019c07ffbf03f1f8d38290b63c8fe522fb1e76f
zanya <tea913636@gmail.com>
2026-01-27 21:18:24 +0800

    chore: add makefile and docx for model part
    add command list:
    help
    bulid-ebpf: build mvp part
    clean: for rust compile folder

---
+++ Makefile
@@ -0,0 +1,13 @@
+.PHONY: help clean
+
+help:  ## Show this help message
+	@echo "Available commands:"
+	@grep -E '^[a-zA-Z_-]+:.*?## .*$$' $(MAKEFILE_LIST) | awk 'BEGIN {FS = ":.*?## "}; {printf "  \033[36m%-20s\033[0m %s\n", $$1, $$2}'
+
+build-ebpf: ## Build ebpf program
+	@echo "start build:"
+	cd service/firewall && cargo +nightly build --package firewall-ebpf --target bpfel-unknown-none -Z build-std=core --release
+
+clean:
+	@echo "clean taregt folder"
+	rm -rf service/firewall/target
--- service/firewall/firewall/src/main.rs
+++ service/firewall/firewall/src/main.rs
@@ -1,3 +1,56 @@
+<<<<<<< Updated upstream
 fn main() {
     println!("Hello, world! for test");
+=======
+use aya::{include_bytes_aligned, Ebpf};
+use aya::maps::RingBuf;
+use aya::programs::{Xdp, XdpFlags};
+use aya_log::EbpfLogger;
+use firewall_common::PacketLog;
+use log::{info, warn};
+use std::net::Ipv4Addr;
+use tokio::io::unix::AsyncFd;
+use tokio::signal;
+use core::ptr;
+
+#[tokio::main]
+async fn main() -> Result<(), anyhow::Error> {
+    env_logger::init();
+
+    #[cfg(debug_assertions)]
+    let mut bpf = Ebpf::load(include_bytes_aligned!(env!("FIREWALL_BPF")))?;
+
+    #[cfg(not(debug_assertions))]
+    let mut bpf = Ebpf::load(include_bytes_aligned!(env!("FIREWALL_BPF")))?;
+
+    if let Err(e) = EbpfLogger::init(&mut bpf) {
+        warn!("failed to initialize eBPF logger: {}", e);
+    }
+
+    let program: &mut Xdp = bpf.program_mut("xdp_firewall").unwrap().try_into()?;
+    program.load()?;
+    program.attach("wlp3s0", XdpFlags::SKB_MODE).unwrap();
+
+    info!("waiting for ctrl+c");
+
+    let mut events: RingBuf<_> = bpf.map_mut("EVENTS").unwrap().try_into()?;
+    let mut poll = AsyncFd::new(events)?;
+
+    loop {
+        let mut guard = poll.readable_mut().await?;
+        let ring_buf = guard.get_inner_mut();
+
+        // 3. 讀取所有可用的事件
+        while let Some(event) = ring_buf.next() {
+            let log = unsafe { ptr::read(event.as_ptr() as *const PacketLog) };
+            println!(
+                "SRC IP: {}, LEN: {}, ACTION: {}",
+                Ipv4Addr::from(log.ip_addr),
+                log.len,
+                log.action
+            );
+        }
+        guard.clear_ready();
+    }
+>>>>>>> Stashed changes
 }
