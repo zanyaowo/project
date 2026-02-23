1646151baea2d3e457946b789f164cbe7bd5c1b4
zanya <tea913636@gmail.com>
2026-02-23 16:33:31 +0800

    feat: load secret key and add attach tc function

--- service/firewall/firewall/src/lib/controller.rs
+++ service/firewall/firewall/src/lib/controller.rs
@@ -1,9 +1,10 @@
 use anyhow::Context;
 use aya::Ebpf;
 use aya::maps::Map;
-use aya::programs::{Xdp, XdpFlags};
+use aya::maps::Array;
+use aya::programs::{Xdp, XdpFlags, tc, SchedClassifier};
 use aya_log::EbpfLogger;
-use log::{warn, info};
+use log::warn;
 
 pub struct FirewallController{
     bpf: Ebpf
@@ -13,6 +14,11 @@ impl FirewallController{
     pub fn load(bytecode: &[u8]) -> anyhow::Result<Self>{
         // 修正：移除 expect，直接使用 ? 處理錯誤
         let mut bpf = Ebpf::load(bytecode)?;
+        let mut secret_map = Array::try_from(
+            bpf.map_mut("SECRET_KEY").context("SECRET_KEY map not found")?
+        )?;
+        let secret = rand::random::<u32>();
+        secret_map.set(0, secret, 0)?;
 
         if let Err(e) = EbpfLogger::init(&mut bpf){
             warn!("failed to initialize eBPF logger: {}", e);
@@ -21,13 +27,23 @@ impl FirewallController{
         Ok(Self{ bpf })
     }
 
-    pub fn attach(&mut self, iface: &str) -> anyhow::Result<()>{
-        let program: &mut Xdp = self.bpf.program_mut("xdp_firewall")
+    pub fn attach_xdp(&mut self, iface: &str) -> anyhow::Result<()>{
+        let xdp_program: &mut Xdp = self.bpf.program_mut("xdp_firewall")
             .context("program not found")?
             .try_into()?;
 
-        program.load().context("failed to load ebpf program")?;
-        program.attach(iface, XdpFlags::SKB_MODE).context("failed to attach ebpf program")?;
+        xdp_program.load().context("failed to load xdp program")?;
+        xdp_program.attach(iface, XdpFlags::SKB_MODE).context("failed to attach ebpf program")?;
+        Ok(())
+    }
+
+    pub fn attach_tc(&mut self , iface: &str) -> anyhow::Result<()>{
+        let _ = tc::qdisc_add_clsact(iface);
+        let program: &mut SchedClassifier = self.bpf.program_mut("tc_egress")
+            .context("tc program not found")?.try_into()?;
+
+        program.load().context("failed to load tc program")?;
+        program.attach(iface, aya::programs::TcAttachType::Egress)?;
         Ok(())
     }
 
