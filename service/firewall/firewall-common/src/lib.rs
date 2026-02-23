e03c10e1eaf6f91963960737e2405d9761e0fc00
zanya <tea913636@gmail.com>
2026-02-23 16:29:33 +0800

    docs: mod

--- service/firewall/firewall-common/src/lib.rs
+++ service/firewall/firewall-common/src/lib.rs
@@ -1,5 +1,9 @@
 #![no_std]
 
+pub mod session;
+pub mod constants;
+
+pub mod protocol;
 #[derive(Copy, Clone)]
 pub struct ModelFeature {
     pub duration: u64, // 持續時間
@@ -20,96 +24,4 @@ pub struct ModelFeature {
     pub _padding: [u8; 6], // 補齊 8 bytes 對齊 (2 bytes u8 + 6 bytes padding = 8 bytes)
 }
 
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct SessionEvent{
-    pub key: SessionKey,
-    pub timestamp: u64,
-    pub len: u16,
-    pub flag: u8,
-    pub padding: [u8; 5],
-}
 
-#[cfg(feature = "user")]
-unsafe impl aya::Pod for SessionEvent {}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct SessionKey {
-    pub src_ip: u32,
-    pub dst_ip: u32,
-    pub src_port: u16,
-    pub dst_port: u16,
-    pub proto: u8,
-    pub _padding: [u8; 3], // 補齊 4 bytes 對齊
-}
-
-#[cfg(feature = "user")]
-unsafe impl aya::Pod for SessionKey {}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct SessionValue {
-    // 累積總流量
-    pub orig_bytes: u64,
-    pub resp_bytes: u64,
-
-    // 累積數量
-    pub orig_pkts: u64,
-    pub resp_pkts: u64,
-
-    // 累積流量（network layer）
-    pub orig_ip_bytes: u64,
-    pub resp_ip_bytes: u64,
-
-    // 時間
-    pub start_ts: u64,
-    pub last_seen_ts: u64,
-
-    // 連線狀態和padding
-    pub flag: u8,
-    pub is_close: bool,
-    pub _padding: [u8; 6], // 補齊 8 bytes 對齊
-}
-
-#[cfg(feature = "user")]
-unsafe impl aya::Pod for SessionValue {}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub enum L4Packet{
-    Tcp(TcpInfo),
-    Udp(UdpInfo),
-    Icmp(IcmpInfo),
-    Unknown,
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct TcpInfo{
-    pub src_port: u16,
-    pub dst_port: u16,
-    pub flags: u8,
-    pub header_len: u64,
-    pub padding: [u8; 3],
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct UdpInfo{
-    pub src_port: u16,
-    pub dst_port: u16,
-    pub header_len: u64,
-    pub padding: [u8; 4],
-}
-
-#[repr(C)]
-#[derive(Copy, Clone)]
-pub struct IcmpInfo {
-    pub icmp_type: u8,
-    pub icmp_code: u8,
-    pub icmp_id: u16,
-    pub icmp_seq: u16,
-    pub header_len: u64,
-    pub padding: [u8; 2],
-}
